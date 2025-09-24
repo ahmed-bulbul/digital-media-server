c206eb32063133ee3ae02cb54b7288dabc76ec12
bulbulahmed <ahmedbulbul.cse@gmail.com>
2025-09-24 19:27:48 +0600

    LEARNING TRAITS

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,10 +1,30 @@
-use core::media::MediaStream;
+// use core::media::MediaStream;
+//
+// fn main() {
+//     let stream = MediaStream {
+//         id: 42,
+//         title: "Rust Demo".to_string(),
+//         duration: 99.0,
+//     };
+//     println!("Loaded {} [{}s]", stream.title, stream.duration);
+// }
+
+
+trait Playable {
+    fn play(&self);
+}
+
+struct Song {
+    title: String,
+}
+
+impl Playable for Song {
+    fn play(&self) {
+        println!("Playing song: {}", self.title);
+    }
+}
 
 fn main() {
-    let stream = MediaStream {
-        id: 42,
-        title: "Rust Demo".to_string(),
-        duration: 99.0,
-    };
-    println!("Loaded {} [{}s]", stream.title, stream.duration);
+    let s = Song { title: "Rust Beats".into() };
+    s.play();
 }
